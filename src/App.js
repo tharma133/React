9650db899ab24f767a3e9c5e892548845f36b003
tharma133 <69578841+tharma133@users.noreply.github.com>
2021-05-06 19:31:40 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { data } from './data'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <div className='header'>
+        <h1>React Projects</h1>
+        <div className='underline'></div>
+      </div>
+      <div className='section-center'>
+        {data.map((item) => {
+          const { id, image, link, title } = item
+          return (
+            <a
+              href={link}
+              key={id}
+              className='container'
+              rel='noreferrer'
+              target='_blank'
+            >
+              <div className='image-wrapper'>
+                <img src={image} alt={title} />
+              </div>
+              <footer>
+                <h2 className='name'>{title}</h2>
+              </footer>
+            </a>
+          )
+        })}
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
